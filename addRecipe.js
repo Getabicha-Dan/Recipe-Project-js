3dcd07e63ad6a4257433a0b962528bfabe499f11
Getabicha Daniel <getabicha.weldekidan@gmail.com>
2022-09-07 13:50:14 +0300

    Check trail

--- addRecipe.js
+++ addRecipe.js
@@ -146,22 +146,28 @@ const editRecipe = (recipes, oldName, newName) => {
 
 // Task: 9. edit a recipe - make it vegan / make it non-vegan
 
-let recipes = [{name: "salmon soup",vegan: false,ingredients: [{name: "salmon",quantity: 1,}, {name: "cream",quantity: 2,}, {name: "milk",quantity: 1,}]}, {
-  name: "carbonara",vegan: false,ingredients: [{name: "pasta",quantity: 1,}, {name: "cheese",quantity: 5,}, {name: "milk",quantity: 1,}]}, {
-    name: "rice bowl",vegan: false,ingredients: [{name: "rice",quantity: 2,}, {name: "water",quantity: 1,}]}];
-
 const toggleVeganStatus = (recipes, recipeName) => {
-    const toBeEditedRecipe = recipes.find(recipe => {
-      if(recipe.nam === recipeName){
-        return recipe;
-      }   
-        return toBeEditedRecipe; 
-    })
-  if (toBeEditedRecipe.vegan === true){
-    return toBeEditedRecipe.vegan = false;
+  const toBeEditedRecipe=recipes.find(recipe=>{
+    if(recipe.name=== recipeName){
+      return recipe;
+    } 
+  })
+  
+  if (toBeEditedRecipe.vegan=== true){
+    return toBeEditedRecipe.vegan= false;
   } else{
     return toBeEditedRecipe.vegan=true;
   }
-}
-
+  }
 console.log(toggleVeganStatus(recipes,"rice bowl"));
+
+// Task: 10. get one recipe by name - return the recipe that matches the exact name of the recipe
+const searchByRecipeName = (recipes, recipeName) => {
+ const myRecipeByName= recipes.filter(recipe=>{
+    if(recipe.name === recipeName){
+      return recipe;
+    }
+    return myRecipeByName;
+  })
+  // use filter
+}
--- trial.js
+++ trial.js
@@ -1,20 +1,25 @@
 // Task: 9. edit a recipe - make it vegan / make it non-vegan
 let recipes=[{name: "salmon soup",vegan: false,ingredients: [{name: "salmon",quantity: 1,}, {name: "cream",quantity: 2,}, {name: "milk",quantity: 1,}]}, {
     name: "carbonara",vegan: false,ingredients: [{name: "pasta",quantity: 1,}, {name: "cheese",quantity: 5,}, {name: "milk",quantity: 1,}]}, {
-      name: "rice bowl",vegan: false,ingredients: [{name: "rice",quantity: 2,}, {name: "water",quantity: 1,}]}];
+      name: "rice bowl",vegan: true,ingredients: [{name: "rice",quantity: 2,}, {name: "water",quantity: 1,}]}];
   
-  const toggleVeganStatus = (recipes, recipeName) => {
-    const toBeEditedRecipe=recipes.find(recipe=>{
-      if(recipe.name=== recipeName){
-        return recipe;
-      }    
+     // Task: 11. search by ingredient name - return all recipes that contain a given ingredient name
+const searchByIngredientName = (recipes, ingredientName) => {
+  const newRecipes=recipes.filter(recipe =>{
+    recipe.ingredients.filter(ingredient =>{
+      if(ingredient.name === ingredientName ){
+        return true;
+      }
+    return recipe;
     })
-    if (toBeEditedRecipe.vegan=== true){
-      return toBeEditedRecipe.vegan= false;
-    } else{
-      return toBeEditedRecipe.vegan=true;
-    }
+   
+  })
+  return newRecipes;
 }
+console.log(searchByIngredientName(recipes,"milk"));
+
+ 
   
-  console.log(toggleVeganStatus(recipes,"rice bowl"));
+  
+
   
